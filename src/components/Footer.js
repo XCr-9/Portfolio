88f0c3568ea9a60ae4be403f87a4b5005d877e54
XCr-9 <152235773+XCr-9@users.noreply.github.com>
2023-12-25 16:56:10 +0900

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
           <h3>Designed and Developed by Ali Kabir</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} KA</h3>
+          <h3>Copyright © {year} AK</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
